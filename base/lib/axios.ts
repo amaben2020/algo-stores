d1203bbfed5f26a7a6b402bead3bf525c47fe942
algomachine007 <amaben202phantom@gmail.com>
2023-03-11 15:00:24 +0100

    [ALGO-003] feat: axios lib update

--- base/lib/axios.ts
+++ base/lib/axios.ts
@@ -4,4 +4,8 @@ export const api = axios.create({
   baseURL: "https://fakestoreapi.com",
 });
 
+// Override timeout default for the library
+// Now all requests using this instance will wait 2.5 seconds before timing out
+api.defaults.timeout = 2500;
+
 export const ENDPOINTS = { products: "/products" };
