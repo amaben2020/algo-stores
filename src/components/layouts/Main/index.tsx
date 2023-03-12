56684d53c04593e22188149fb307987097d2f4dc
algomachine007 <amaben202phantom@gmail.com>
2023-03-12 10:23:08 +0100

    [ALGO-004] feat: building a design system

--- src/components/layouts/Main/index.tsx
+++ src/components/layouts/Main/index.tsx
@@ -4,9 +4,9 @@ import Header from "@/components/sections/Header";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div className="min-h-screen mx-auto max-w-2xl">
+      <div className="min-h-screen mx-auto max-w-20xl">
         <Header />
-        <main className="container mx-auto px-4 sm:px-6">{children}</main>
+        <main className="mx-auto p-2 md:p-6">{children}</main>
         <Footer />
       </div>
     </>
--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -1,4 +1,4 @@
-import Logo from "@/components/elements/logo";
+import Logo from "@/components/elements/Logo";
 import { MoonIcon, SunIcon } from "@heroicons/react/solid";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
@@ -16,28 +16,30 @@ const Header = () => {
 
     const currentTheme = theme === "system" ? systemTheme : theme;
 
-    if (currentTheme === "dark") {
-      return (
-        <SunIcon
-          className="w-10 h-10 text-yellow-500"
-          role="button"
-          onClick={() => setTheme("light")}
-        />
-      );
-    } else {
-      return (
-        <MoonIcon
-          className="w-10 h-10 text-gray-900"
-          role="button"
-          onClick={() => setTheme("dark")}
-        />
-      );
+    switch (currentTheme) {
+      case "dark":
+        return (
+          <SunIcon
+            className="w-10 h-10 text-yellow-500"
+            role="button"
+            onClick={() => setTheme("light")}
+          />
+        );
+
+      default:
+        return (
+          <MoonIcon
+            className="w-10 h-10 text-gray-900"
+            role="button"
+            onClick={() => setTheme("dark")}
+          />
+        );
     }
   };
 
   return (
-    <header className="none h-15 shadow-sm dark:border-gray-700">
-      <div className="px-4 sm:px-6 py-4 flex justify-between items-center">
+    <header className="h-15 shadow-sm dark:border-gray-700">
+      <div className="px-1 sm:px-2 py-4 flex justify-between items-center">
         <Logo />
 
         {renderThemeChanger()}
