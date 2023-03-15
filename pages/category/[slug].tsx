acc5ec00fb98fe6d9c16a27954b5fa6f4711c12b
algomachine007 <amaben202phantom@gmail.com>
2023-03-15 10:59:05 +0100

    [ALGO-006] feat: building the category page

--- pages/category/[slug].tsx
+++ pages/category/[slug].tsx
@@ -1,18 +1,37 @@
+import ImageComponent from "@/components/elements/Image/ImageComponent";
 import Layout from "@/components/layouts/Page";
 import { GetServerSidePropsContext } from "next";
 import { ENDPOINTS, api } from "~/base/lib/axios";
 import IProducts from "~/types/types";
 
-const Category = ({ products }: { products: IProducts[] }) => {
+const Category = ({
+  products,
+  type,
+}: {
+  products: IProducts[];
+  type: string;
+}) => {
   return (
     <Layout>
-      <div>
-        {products.map((product) => (
-          <div key={product.id}>
-            <h2>{product.title}</h2>
+      <h1 className="text-xl">
+        {type.split("")[0].toUpperCase() + type.substring(1)} Category
+      </h1>
+      {products.map((product) => (
+        <div key={product.id} className="flex p-6 my-6 border rounded">
+          <div className="w-1/3">
+            <ImageComponent
+              title={product.title}
+              image={product.image}
+              width={300}
+              height={300}
+            />
           </div>
-        ))}
-      </div>
+          <div className="ml-4 space-y-3">
+            <h2>{product.title}</h2>
+            <h3>{product.description}</h3>
+          </div>
+        </div>
+      ))}
     </Layout>
   );
 };
@@ -31,6 +50,7 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   return {
     props: {
       products: filteredProduct,
+      type: params?.slug,
     },
   };
 };
