b2e422bfc920aad9215c2b21d1eeb0f2a5909f63
algomachine007 <amaben202phantom@gmail.com>
2023-03-16 16:50:32 +0100

    [ALGO-007] fix: adding loading skeleton to redux

--- app/redux/store/store.ts
+++ app/redux/store/store.ts
@@ -1,15 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+
+import { persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
 import cartReducer from "../features/cart/cart-slice";
 import userReducer from "../features/user/user-slice";
+
+const persistConfig = {
+  key: "cart",
+  timeout: 1,
+  storage: storage,
+  blacklist: ["cart"],
+};
+
+const persistedReducer = persistReducer(persistConfig, cartReducer);
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    cart: cartReducer,
+    cart: persistedReducer,
   },
+
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+
 export type AppDispatch = typeof store.dispatch;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,24 @@
+import HomePageLoading from "@/components/sections/HomePageLoading";
 import "flowbite";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { AppProps } from "next/app";
 import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 import "tailwindcss/tailwind.css";
 import { store } from "~/app/redux/store/store";
 import "../styles/globals.css";
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const persistor = persistStore(store);
   return (
     <Provider store={store}>
       <SessionProvider session={session}>
         <ThemeProvider enableSystem={true} attribute="class">
-          <Component {...pageProps} />
+          <PersistGate loading={<HomePageLoading />} persistor={persistor}>
+            <Component {...pageProps} />
+          </PersistGate>
         </ThemeProvider>
       </SessionProvider>
     </Provider>
